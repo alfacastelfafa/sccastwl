82a2e25addd19aa6a551ebfc87831af9afae6812
Alfacastel <alfacastel3@gmail.com>
2022-10-16 22:00:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6289675850011']
+global.ownernomer = "6289675850011"
+global.premium = ['6289675850011']
+global.packname = 'Sticker CastwlBot'
+global.author = 'ByFafago1k'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
